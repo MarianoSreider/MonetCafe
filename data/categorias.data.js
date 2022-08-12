ad49a4445aa50d77fda93c0961cd90ca18094c93
marsre <mariano.a.sreider@gmail.com>
2022-08-12 14:26:43 -0300

    tercera

--- data/categorias.data.js
+++ data/categorias.data.js
@@ -2,17 +2,17 @@ const categorias = [
     {
         id:1,
         nombre:"Café",
-        slug: "Galeria (3)"
+        slug: "Galeria3"
     }, 
     {
         id:2,
         nombre:"Pattiserie",
-        slug: "Galeria (1)"
+        slug: "Galeria1"
     },
     {
         id:3,
         nombre:"Accesorios",
-        slug: "Galeria (2)"
+        slug: "Galeria2"
     },
    
 
